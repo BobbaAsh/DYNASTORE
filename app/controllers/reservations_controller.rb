c5dd5e671bdff7627ce763d0d805de7340d2127a
stevie cutman <steviecut@hotmail.fr>
2019-05-28 12:46:32 +0200

    creation route et page Reservation

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,22 @@
+class ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.all
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Reservation.create!(artist_id: 1, user_id: 1, status: "pending", final_price: 12.00 )
