7b857bc26fee3ce5ba066671e7b8cc532aeccac9
Daniel Kaddous <49636707+BobbaAsh@users.noreply.github.com>
2019-05-28 16:07:20 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   get 'reservations/new', to: 'reservations#new'
   get 'reservations/:id', to: "reservations#show"
   post 'reservations', to: "reservations#create"
-
-#   get 'reservations/update'
-#   get 'reservations/delete'
+  get 'reservations/update'
+  get 'reservations/delete'
   devise_for :users
   root to: 'pages#home'
+    resources :users
     resources :artists do
     resources :reservations
-end
+  end
 end
