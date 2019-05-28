b82f1d0d11a474641c04f7937d2c9c89d578bf8e
Daniel Kaddous <danielkaddous.ys@gmail.com>
2019-05-28 15:00:43 +0200

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts 'Creating artist...'
-artists_attributes = [
-  {
-    name:         'Wiz Khalifa',
-    genre:      'Rap',
-    price:  15000,
-    description:  'Rapper who made the bager "Black & Yellow"'
-  },
-    {
-    name:         'Travis Scoot',
-    genre:      'Rap',
-    price:  20000,
-    description:  'He released Astroworld last year'
-  },
-      {
-    name:         'Da Baby',
-    genre:      'Rap',
-    price:  10000,
-    description:  'Da Baby Da Baby on the pony'
-  },
-]
-Artist.create!(artists_attributes)
