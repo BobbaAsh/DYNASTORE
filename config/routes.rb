76f2de361f0038991e9806d25968c1aee3cad514
stevie cutman <steviecut@hotmail.fr>
2019-05-30 17:17:29 +0200

    destroy working

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
     resources :reservations, only: [:new, :create]
  end
 
- resources :reservations, only: [:index, :show, :edit, :update, :delete]
+ resources :reservations, only: [:index, :show, :edit, :update, :destroy]
 end
