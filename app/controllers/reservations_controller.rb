49d48902f315281305dd0d2335148bf67c91bda5
stevie cutman <steviecut@hotmail.fr>
2019-05-30 15:34:50 +0200

    Added new reservaation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -12,7 +12,7 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = Reservation.new
+    @reservation = Reservation.new(reservation_params)
     if @reservation.save
       redirect_to reservation_path(@reservation.id)
     else
@@ -21,17 +21,23 @@ class ReservationsController < ApplicationController
   end
 
   def edit
+    @reservation = Reservation.find(params[:id])
   end
 
   def update
+    @reservation = Reservation.find(params[:id])
+    @reservation.update(reservation_params)
+    redirect_to @reservation
   end
 
   def delete
+    @reservation.destroy
+    redirect_to reservation_url, notice: 'Your reservation was successfully destroyed !'
   end
 
   private
 
   def reservation_params
-    params.require(:user).permit(:user_id)
+    params.require(:user).permit(:user_id, :begin_date, :end_date, :status)
   end
 end
